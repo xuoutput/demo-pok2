9316f74356f64fe976bfcef2c20e17b807509aed
xuoutput <847438919@qq.com>
2025-04-05 11:35:47 +0800

    feat(help): add version and help message

--- packages/pok/bin/cli.js
+++ packages/pok/bin/cli.js
@@ -1,2 +1,17 @@
 #!/usr/bin/env node
-console.log("hello pokeman");
+const { description, name, version } = require("../package.json");
+
+const options = process.argv.slice(2);
+
+const HELP_MESSAGE = `${name} ${version}
+${description}
+
+Usage: 
+--help    Help documentation
+--version Installed package version`;
+
+if (options.includes("--version")) {
+  console.log(version);
+} else {
+  console.log(HELP_MESSAGE);
+}
